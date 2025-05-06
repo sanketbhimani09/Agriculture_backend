99b1a48dd926b822ab827077e0024bdd55ee7371
Sanket Bhimani <sanketbhimani92@gmail.com>
2025-05-06 12:57:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const technologyRoutes = require('./Technology/technology'); // Correct path
+const categoriesRoutes = require('./ProductCategory/category');
+const productRoutes = require('./Products/products');
+const userRoutes = require('./User/user');
+const adminRoutes=require('./Admin/admin');
+const cartRoutes=require('./Cart/cart');
+const pendingOrders=require('./Orders/Orders');
+const messages=require('./ContactUs/message');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/griculture").then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+// Use the all routes
+app.use(technologyRoutes);
+app.use(categoriesRoutes);
+app.use(productRoutes);
+app.use(userRoutes);
+app.use(adminRoutes);
+app.use(cartRoutes);
+app.use(pendingOrders);
+app.use(messages);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running  at http://localhost:${port}`);
+});
